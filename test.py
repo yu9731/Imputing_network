887b7a50f30420a372f6ef7494d3f4aba6db3719
yu9731 <84125268+yu9731@users.noreply.github.com>
2025-09-27 03:26:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import layers
 
-building_lst = ['AT_SFH']
-
+building = 'AT_SFH'
 pred_hour = 24
-length = 24*365
+length = 24 * 365
 windows_length = 24
 variable_num = 4
 temporal_num = 1
@@ -16,53 +15,44 @@ epoch = 200
 cnt = 0
 
 def test(building):
-    generator = load_model(f'generator_after_discriminator_{building}.h5')
-    
+    generator = load_model(f'Baseline_Model/generator_{building}.h5')
+
     test_for_plot = []
     ori_test = []
 
-    def get_points():
+    def get_points_1():
         points = []
         mask = []
         for i in range(1):
-            m = np.zeros((pred_hour, variable_num, 1), dtype=np.uint8)  # 1D
+            m = np.zeros((pred_hour, variable_num, 1), dtype=np.uint8)  # 24,4,1
             x1 = np.random.randint(0, pred_hour - pred_hour + 1, 1)
 
-            m[:, variable_num-temporal_num-1] = 1
+            m[:, -2] = 1
             mask.append(m)
         return np.array(mask)
 
-    for i in range((length-pred_hour)//windows_length+1):
-        data = np.load(f'train_data/{pred_hour}/task_{building}_data_solar_test_{pred_hour}.npy')
-        data = data.reshape((data.shape[0], data.shape[1], data.shape[2]))
-        data = data[i]
+    for i in range((length - pred_hour) // windows_length + 1):
+        data_1 = np.load(f'train_data/test_{building}.npy')
+        data_1 = data_1[i]
 
-        data = data.reshape(1, pred_hour, variable_num, 1)
+        data_1 = data_1.reshape(1, pred_hour, variable_num, 1)
         mask_batch = get_points_1()
-        generator_input = data * (1 - mask_batch)
+        generator_input_1 = data_1 * (1 - mask_batch)
 
-        test_pred = generator.predict(generator_input)
+        test_pred_1 = generator.predict(generator_input_1)
 
-        for j in range(test_pred_baseline.shape[1]):
-            if pred_hour == 24:
-                test_for_plot.append(test_pred[0][j][variable_num-temporal_num-1][0])
-                ori_test.append(data[0][j][variable_num-temporal_num-1])
-            else:
-                if i == 0:
-                    test_for_plot.append(test_pred[0][j][variable_num-temporal_num-1][0])
-                    ori_test.append(data[0][j][variable_num-temporal_num-1][0])
-                else:
-                    if j >= (pred_hour - 24):
-                        test_for_plot.append(test_pred[0][j][variable_num-temporal_num-1][0])
-                        ori_test.append(data[0][j][variable_num-temporal_num-1][0])
-                    else:
-                        pass
-    nrmse = np.sqrt(np.mean((np.array(test_for_plot)-np.array(ori_test))**2)) / (np.max(np.array(ori_test)) - np.min(np.array(ori_test)))
+        for j in range(test_pred_1.shape[1]):
+            test_for_plot.append(test_pred_1[0][j][variable_num - temporal_num - 1])
+            ori_test.append(data_1[0][j][variable_num - temporal_num - 1])
 
-    min_data = np.load(f'{building}_min.npy')
-    max_data = np.load(f'{building}_max.npy')
-    test_for_plot = np.array(test_for_plot) * (max_data - min_data) + min_data
-    ori_test = np.array(ori_test) * (max_data - min_data) + min_data
+    max = np.load(f'train_data/{building}_max.npy')
+    min = np.load(f'train_data/{building}_min.npy')
+
+    test_for_plot = np.array(test_for_plot) * (max - min) + min
+    ori_test = np.array(ori_test) * (max - min) + min
+
+    nrmse = np.sqrt(np.mean((np.array(test_for_plot) - np.array(ori_test)) ** 2)) / (
+            np.max(np.array(ori_test)) - np.min(np.array(ori_test)))
 
     plt.plot(test_for_plot, label='Pred')
     plt.plot(ori_test, label='Ori')
@@ -71,5 +61,6 @@ def test(building):
 
     return nrmse
 
-nrmse = test(building, 'global', dilation=True, UNet=True)
+nrmse = test(building)
 print(nrmse)
+
