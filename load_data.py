4efdfe1eb48ab10f5ee761a95da04f660742a5ca
yu9731 <84125268+yu9731@users.noreply.github.com>
2025-09-27 03:26:01 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -38,8 +38,8 @@ def filtering_by_hours(train_data, pd_time, Timestamp, save_file=False):
                 pd_time.loc[outlier_index, 'Energy [KW]'] = np.percentile(energy_demand, 50)
 
     return pd_time
-'''
-variable_name = ['AT_SFH', 'temp', 'solarradiation']
+
+variable_name = ['temp', 'solarradiation', 'AT_SFH']
 pred_hour = 24
 windows_length = 24
 
@@ -50,8 +50,6 @@ heat_data.index = heat_data_index
 
 pd_train_data = heat_data.loc['2016-01-01 00:00:00':'2017-12-31 23:00:00', variable_name]
 pd_test_data = heat_data.loc['2018-01-01 00:00:00':'2018-12-31 23:00:00', variable_name]
-
-# weather_data_test = weather_data_.loc['2018-01-01 00:00:00':'2018-12-31 23:00:00', [f'{region[i]}_temperature', f'{region[i]}_radiation_direct_horizontal']]
 
 pd_time = pd_time_information(pd.to_datetime(pd_train_data.index))
 np_hour = pd_time.loc[:, 'hour'].values
@@ -71,7 +69,7 @@ train_data = np.hstack((pd_train_data.values, np_hour))
 test_data = np.hstack((pd_test_data.values, np_hour_test))
 
 max_data = np.max(train_data)
-min_data = np.min(test_data)
+min_data = np.min(train_data)
 
 np.save(f'train_data/AT_SFH_max.npy', max_data)
 np.save(f'train_data/AT_SFH_min.npy', min_data)
@@ -96,39 +94,49 @@ data_test = data_test.reshape((data_test.shape[0], data_test.shape[1], data_test
 
 np.save(f'train_data/train_AT_SFH.npy', data_train)
 np.save(f'train_data/test_AT_SFH.npy', data_test)
-'''
 
-variable_name = ['L17', 'temp', 'solarradiation']
+variable_name = ['temp', 'solarradiation', 'L17']
 pred_hour = 24
 windows_length = 24
 
 heat_data = pd.read_csv(f'L17.csv', index_col=[0], parse_dates=True) #.astype('float')
 heat_data_test = pd.read_csv(f'L17_test.csv', index_col=[0], parse_dates=True) #.astype('float')
 
-heat_data = heat_data.loc[:, 'L17'].astype('float')
+# heat_data = heat_data.loc[:, 'L17'].astype('float')
 heat_data_index = heat_data.index
 heat_data_index = pd.to_datetime(heat_data.index)
 heat_data.index = heat_data_index
 
 pd_train_data = heat_data.loc['2022-10-03 23:00:00': '2023-03-31 23:00:00', variable_name]
-pd_test_data = heat_data.loc['2023-12-01 00:00:00':'2023-12-31 23:00:00', variable_name]
+pd_test_data = heat_data_test.loc['2023-12-01 00:00:00':'2023-12-31 23:00:00', variable_name]
 
 pd_time = pd_time_information(pd.to_datetime(pd_train_data.index))
-pd_time = filtering_by_hours(pd_train_data, pd_time, pd_time.index)
+pd_time = filtering_by_hours(pd_train_data.values[:,-1], pd_time, pd_time.index)    # numpy from pd_train_data instead of pd_train_data
+
 np_hour = pd_time.loc[:, 'hour'].values
+heat_load_train = pd_time.loc[:, 'Energy [KW]'].values
 
 pd_time_test = pd_time_information(pd.to_datetime(pd_test_data.index))
-pd_time_test = filtering_by_hours(pd_test_data, pd_time_test, pd_time_test.index)
-np_hour_test = pd_time_test.loc[:, 'hour'].values
+pd_time_test = filtering_by_hours(pd_test_data.values[:,-1], pd_time_test, pd_time_test.index)
 
-train_data = np.hstack((pd_train_data.values, np_hour))
-test_data = np.hstack((pd_test_data.values, np_hour_test))
+np_hour_test = pd_time_test.loc[:, 'hour'].values
+heat_load_test = pd_time_test.loc[:, 'Energy [KW]'].values
+
+train_data = np.hstack((pd_train_data.values[:,:2], heat_load_train.reshape((heat_load_train.shape[0],1)), np_hour.reshape((np_hour.shape[0],1))))
+test_data = np.hstack((pd_test_data.values[:,:2], heat_load_test.reshape((heat_load_test.shape[0],1)), np_hour_test.reshape((np_hour_test.shape[0],1))))
 
 max_data = np.max(train_data)
-min_data = np.min(test_data)
+min_data = np.min(train_data)
 
 np.save(f'train_data/L17_max.npy', max_data)
 np.save(f'train_data/L17_min.npy', min_data)
+
+for j in range(train_data.shape[1]):
+    #min_data = np.min(train_data[:, j])
+    #max_data = np.max(train_data[:, j])
+
+    train_data[:, j] = (train_data[:, j] - min_data) / (max_data - min_data)
+    test_data[:, j] = (test_data[:, j] - min_data) / (max_data - min_data)
 
 data_train = np.zeros(((train_data.shape[0]-pred_hour)//windows_length+1, pred_hour, 4))
 for j in range((train_data.shape[0]-pred_hour)//windows_length+1):
@@ -145,4 +153,5 @@ for j in range((test_data.shape[0]-pred_hour)//windows_length+1):
 data_test = data_test.reshape((data_test.shape[0], data_test.shape[1], data_test.shape[2], 1))
 
 np.save(f'train_data/train_L17.npy', data_train)
+np.save(f'train_data/test_L17.npy', data_test)
 np.save(f'train_data/test_L17.npy', data_test)
